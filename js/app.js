09832080f0230d73bbd3356bf5407b38b9d12d09
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-03-27 21:34:07 -1000

    updated

--- js/app.js
+++ js/app.js
@@ -6,14 +6,19 @@ Also don't forget to run your live-server for this assignment.*/
 /*Despite being the cat's meow, Kapiolani Coffee Shop's current rating is n/a. Let's keep the hungry fans happy by changing the current ranking to:
 
 Rating: 5 Stars*/
-
+var changeRating = document.getElementById("rating1");
+changeRating.innerHTML = "Rating: 5 Stars";
 
 //2. Sunrise Restaurant
 /*No rise and shine for the Sunrise Restaurant, as their price and address needs some serious updating. Bring in some sunshine by changing the price and address to the following:
 
 Price: $$$
 Adress: 525 Kapahulu Ave, Honolulu, HI 96815*/
+var changePrice = document.getElementById("price2");
+changePrice.innerHTML = "Price: $$$";
 
+var changeAddress = document.getElementById("addy2");
+changeAddress.innerHTML = "Address: 525 Kapahulu Ave, Honolulu, Hi 96815";
 
 //3. Ramen Nakamura
 /*Literally, where did the time go? Ramen Nakamura is missing the hours section! This mistake is timeless.
@@ -22,7 +27,13 @@ You will need to create a new div element with an id of "hours3". Inside this di
 
 Hours: 6pm - 12am Sun - Sat
 
+
 Append this div element to the div id "text3"*/
+
+var addHours = document.createElement("div");
+addHours.id = "hours3";
+addHours.innerHTML = "Hours: 6pm -12am Sun - Sat"
+text3.appendChild(addHours);
 
 
 //4. Kam Bowl
@@ -34,6 +45,10 @@ Price: $$$
 
 Append this element to the div with the id of 
 "text4"*/
+var addPrice = document.createElement("div");
+addPrice.id = "price4";
+addPrice.innerHTML = "Price: $$$";
+text4.appendChild(addPrice);
 
 /*Create a new div element with an id of "addy4". Inside this div element, give it the following info:
 
@@ -41,6 +56,10 @@ Address: 1620 N. School St, Honolulu, HI 96817
 
 Append this element to the div with the id of 
 "text4"*/
+var addAddress = document.createElement("div");
+addAddress.id = "addy4";
+addAddress.innerHTML = "Address: 1620 N. School St, Honolulu, Hi 96817";
+text4.appendChild(addAddress);
 
 /*Create a new div element with an id of "hours4". Inside this div element, give it the following info:
 
@@ -49,13 +68,22 @@ Hours: 9am - 10pm Sun - Sat
 Append this element to the div with the id of 
 "text4"
 */
+var addHours2 = document.createElement("div");
+addHours2.id = "hours4";
+addHours2.innerHTML = "Hours: 9am - 10pm Sun - Sat";
+text4.appendChild(addHours2);
+
+
 
 /*Create a paragraph element with an id of "update4". Inside this paragraph element, give it the following info:
 
 I'd leave a blank space for this oxtail soup!
 
 Append this element to the div id of "comment4"*/
-
+var addParagraph = document.createElement("p");
+addParagraph.id = "update4";
+addParagraph.innerHTML = "I'd leave a blank space for this oxtail soup!";
+comment4.appendChild(addParagraph);
 
 //5. The Death Star Cafe
 /*The empire strikes back with a new and improved oxtail soup offering. Help the dark side win back customers by executing the following commands:*/
@@ -79,3 +107,27 @@ hours5: Hours: We never close.
 Append the div elements to the div id "text5"
 */
 
+var updateName5 = document.createElement("div");
+updateName5.id = "name5";
+updateName5.innerHTML = "5. The Death Star Cafe";
+text5.appendChild(updateName5);
+
+var updateRating5 = document.createElement("div");
+updateRating5.id = "rating5";
+updateRating5.innerHTML = "Rating: 2 Stars";
+text5.appendChild(updateRating5);
+
+var updatePrice5 = document.createElement("div");
+updatePrice5.id = "price5";
+updatePrice5.innerHTML = "Price: $$";
+text5.appendChild(updatePrice5);
+
+var updateAddy5 = document.createElement("div");
+updateAddy5.id = "addy5";
+updateAddy5.innerHTML = "Address: A galaxy far, far away.";
+text5.appendChild(updateAddy5);
+
+var updateHours5 = document.createElement("div");
+updateHours5.id = "hours5";
+updateHours5.innerHTML = "Hours: We never close";
+text5.appendChild(updateHours5);
